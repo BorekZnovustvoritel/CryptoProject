773e8fd71f0cfe2924bd51f45168c41cbb3163c3
Marek Szymutko <xszymu01@vutbr.cz>
2021-01-27 12:58:29 +0100

    DH now works with real-sized modulo

--- diffieHellman.py
+++ diffieHellman.py
@@ -2,13 +2,25 @@ from random import randint
 from discreteLogarithm import disLog
 from squareAndMultiply import squareAndMultiply
 from groups import getElementDegree
+from primeGen import primeGen
 
 def crack(g, mod, A, B):
     a = disLog(A, g, mod)
     return squareAndMultiply(B, a, mod)
 
-def setKey(g, mod, a = None, b = None):
-    q = getElementDegree(g, mod)
+def setKey(g = None, modLength = None, mod = None, a = None, b = None):
+    if not mod:
+        if not modLength:
+            modLength = 2048
+        print("Generování prvočísla o délce %d bitů..." % modLength)
+        mod = primeGen(modLength)
+        print("Hotovo!")
+    if not g:
+        g = randint(2, mod - 1)
+    if modLength < 124:
+        q = getElementDegree(g, mod)
+    else:
+        q = g
     if not a:
         a = randint(2, q)
     A = squareAndMultiply(g, a, mod)
@@ -17,13 +29,24 @@ def setKey(g, mod, a = None, b = None):
     B = squareAndMultiply(g, b, mod)
     keyA = squareAndMultiply(B, a, mod)
     keyB = squareAndMultiply(A, b, mod)
-    return a, b, A, B, keyA, keyB
+    return mod, g, a, b, A, B, keyA, keyB
 
 if __name__ == "__main__":
     mode = int(input("Zadejte kód operace:\n'1' pro vytvoření klíče\n'2' pro prolomení\n"))
     if mode == 1:
-        g = int(input("Zadejte hodnotu prvku: "))
-        mod = int(input("Zadejte prvočíselné modulo grupy: "))
+        try:
+            g = int(input("Zadejte hodnotu prvku (Nechte prázdné pro náhodně zvolené g): "))
+        except ValueError:
+            g = None
+        try:
+            mod = int(input("Zadejte prvočíselné modulo grupy (Nechte prázdné pro náhodně zvolené modulo): "))
+            modLength = None
+        except ValueError:
+            mod = None
+            try:
+                modLength = int(input("Zadejte bitovou délku modula grupy (Nechte prázdné pro délku 2048 b): "))
+            except ValueError:
+                modLength = None
         try:
             a = int(input("Zadejte hodnotu a (Nechte prázdné pro náhodně zvolené a): "))
         except ValueError:
@@ -32,8 +55,8 @@ if __name__ == "__main__":
             b = int(input("Zadejte hodnotu b (Nechte prázdné pro náhodně zvolené b): "))
         except ValueError:
             b = None
-        a, b, A, B, keyA, keyB = setKey(g, mod, a, b)
-        print("a = %d, b = %d, A = %d, B = %d, keyA = %d, keyB = %d" % (a, b, A, B, keyA, keyB))
+        mod, g, a, b, A, B, keyA, keyB = setKey(g, modLength, mod, a, b)
+        print("mod = %d\ng = %d\na = %d\n b = %d\n A = %d\n B = %d\n keyA = %d\n keyB = %d" % (mod, g, a, b, A, B, keyA, keyB))
     elif mode == 2:
         g = int(input("Zadejte prvek g: "))
         mod = int(input("Zadejte prvočíselné modulo grupy: "))
