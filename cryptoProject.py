9f0e4d2320dc9c2628b1dc4b561bce794344f26d
Marek Szymutko <xszymu01@vutbr.cz>
2021-02-07 16:22:16 +0100

    RSA somewhat done

--- cryptoProject.py
+++ cryptoProject.py
@@ -27,8 +27,55 @@ while option != len(options) + 1:
                 mod, Pk, Sk = ans
                 print("mod = %d\nPk = %d\nSk = %d\nVýpočet trval %.2f sekund." % (mod, Pk, Sk, time))
                 input()
-            #elif option == 2:
+            elif option1 == 2:
                 #zašifrování
+                if Pk != 0 and Sk != 0 and mod != 0:
+                    options2 = ("Použít hodnoty v paměti", "Vložit vlastní hodnoty\nHodnoty v paměti jsou:\nmod = %d\nPk = %d\nSk = %d" % (mod, Pk, Sk))
+                    option2 = menu("RSA - šifrování:", options2)
+                    if option2 == 1:
+                        #použít uložené hodnoty
+                        mess = inp(int, "Zadejte číslo k zašifrování:\n", "Zkuste to znovu:\n")
+                        encr = rsa.encrypt(mess, Pk, mod)
+                        print("Zašifrované číslo je:\n%d" % encr)
+                        input()
+                    elif option2 == 2:
+                        #použít nové hodnoty
+                        mod = inp(int, "Zadejte modulo kryptosystému:\n", "Zkuste to znovu:\n")
+                        Pk = inp(int, "Zadejte veřejný klíč:\n", "Zkuste to znovu:\n")
+                        mess = inp(int, "Zadejte číslo k zašifrování", "Zkuste to znovu")
+                        encr = rsa.encrypt(mess, Pk, mod)
+                        print("Zašifrované číslo je:\n%d" % encr)
+                        input()
+                else:
+                    #použít nové hodnoty
+                    mod = inp(int, "Zadejte modulo kryptosystému:\n", "Zkuste to znovu:\n")
+                    Pk = inp(int, "Zadejte veřejný klíč:\n", "Zkuste to znovu:\n")
+                    mess = inp(int, "Zadejte číslo k zašifrování", "Zkuste to znovu")
+                    encr = rsa.encrypt(mess, Pk, mod)
+                    print("Zašifrované číslo je:\n%d" % encr)
+                    input()
+            elif option1 == 3:
+                #dešifrování
+                if Pk != 0 and Sk != 0 and mod != 0:
+                    options2 = ("Použít hodnoty v paměti", "Vložit vlastní hodnoty\nHodnoty v paměti jsou:\nmod = %d\nPk = %d\nSk = %d" % (mod, Pk, Sk))
+                    option2 = menu("RSA - dešifrování:", options2)
+                    if option2 == 1:
+                        #použít uložené hodnoty
+                        tempEncr = inp(int, "Zadejte číslo k dešifrování. Nechte prázdné pro použití zprávy z paměti.\nZašifrované zpráva v paměti:\n%d\n" % encr, "", True)
+                        if tempEncr:
+                            encr = tempEncr
+                        mess = rsa.decrypt(encr, Sk, mod)
+                        print("Dešifrovaná zpráva je:\n%d" % mess)
+                        input()
+                    elif option2 == 2:
+                        #použít nové hodnoty
+                        mod = inp(int, "Zadejte modulo kryptosystému:\n", "Zkuste to znovu:\n")
+                        Sk = inp(int, "Zadejte soukromý klíč:\n", "Zkuste to znovu:\n")
+                        encr = inp(int, "Zadejte číslo k dešifrování", "Zkuste to znovu")
+                        mess = rsa.decrypt(encr, Sk, mod)
+                        print("Dešifrovaná zpráva je:\n%d" % encr)
+                        input()
+                
             option1 = menu("RSA", options1)
     #elif option == 2:
         #dh
