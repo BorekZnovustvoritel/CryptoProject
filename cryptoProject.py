352ff1aaa9fd13f992df927e8561e9d7b2fb0c3b
Marek Szymutko <xszymu01@vutbr.cz>
2021-07-20 19:58:00 +0200

    Proč píšu kód česky a commit notes anglicky? Nicméně jsem zprovoznil vše, co je zatím implementované v "cryptoProject.py" a využil jsem k tomu konečně metodu inp z "functionality//inp.py" (Jsem na ni hrdý). Konečně je to half-finised kód a ne dumpster fire, yaay.

--- cryptoProject.py
+++ cryptoProject.py
@@ -1,4 +1,4 @@
-
+import runpy
 #^Místo pro základní Python moduly
 #try:
 from functionality.inp import inp
@@ -6,6 +6,7 @@ from functionality.menu import menu
 import algorithms.rsa as rsa
 import algorithms.diffieHellman as dh
 from functionality.stopwatch import stopwatch
+import basics
 #^Místo pro vlastní moduly
 options = ("RSA", "Diffie-Hellman", "Základní operace")
 option = menu("Hlavní menu:", options)
@@ -27,7 +28,7 @@ while option != len(options) + 1:
                 ans, time = stopwatch(rsa.generate, bitLength = modBitLength, publicKey = Pk)
                 mod, Pk, Sk = ans
                 print("mod = %d\nPk = %d\nSk = %d\nVýpočet trval %.2f sekund." % (mod, Pk, Sk, time))
-                input()
+                input("Stiskněte enter pro pokračování.")
             elif option1 == 2:
                 #zašifrování
                 if Pk != 0 and Sk != 0 and mod != 0:
@@ -38,7 +39,7 @@ while option != len(options) + 1:
                         mess = inp(int, "Zadejte číslo k zašifrování:\n", "Zkuste to znovu:\n")
                         encr = rsa.encrypt(mess, Pk, mod)
                         print("Zašifrované číslo je:\n%d" % encr)
-                        input()
+                        input("Stiskněte enter pro pokračování.")
                     elif option2 == 2:
                         #použít nové hodnoty
                         mod = inp(int, "Zadejte modulo kryptosystému:\n", "Zkuste to znovu:\n")
@@ -46,7 +47,7 @@ while option != len(options) + 1:
                         mess = inp(int, "Zadejte číslo k zašifrování", "Zkuste to znovu")
                         encr = rsa.encrypt(mess, Pk, mod)
                         print("Zašifrované číslo je:\n%d" % encr)
-                        input()
+                        input("Stiskněte enter pro pokračování.")
                 else:
                     #použít nové hodnoty
                     mod = inp(int, "Zadejte modulo kryptosystému:\n", "Zkuste to znovu:\n")
@@ -54,7 +55,7 @@ while option != len(options) + 1:
                     mess = inp(int, "Zadejte číslo k zašifrování", "Zkuste to znovu")
                     encr = rsa.encrypt(mess, Pk, mod)
                     print("Zašifrované číslo je:\n%d" % encr)
-                    input()
+                    input("Stiskněte enter pro pokračování.")
             elif option1 == 3:
                 #dešifrování
                 if Pk != 0 and Sk != 0 and mod != 0:
@@ -75,11 +76,12 @@ while option != len(options) + 1:
                         encr = inp(int, "Zadejte číslo k dešifrování", "Zkuste to znovu")
                         mess = rsa.decrypt(encr, Sk, mod)
                         print("Dešifrovaná zpráva je:\n%d" % encr)
-                        input()
+                        input("Stiskněte enter pro pokračování.")
                 
             option1 = menu("RSA", options1)
     elif option == 2:
         #dh
+        modLength = 0
         mod = 0
         generator = 0
         a = 0
@@ -88,35 +90,42 @@ while option != len(options) + 1:
         B = 0
         keyAlice = 0
         keyBob = 0
-        try:
-            g = int(input("Zadejte hodnotu prvku (Nechte prázdné pro náhodně zvolené g): "))
-        except ValueError:
-            g = None
-        try:
-            mod = int(input("Zadejte prvočíselné modulo grupy (Nechte prázdné pro náhodně zvolené modulo): "))
-            modLength = None
-        except ValueError:
-            mod = None
-            try:
-                modLength = int(input("Zadejte bitovou délku modula grupy (Nechte prázdné pro délku 2048 b): "))
-            except ValueError:
-                modLength = None
-        try:
-            a = int(input("Zadejte hodnotu a (Nechte prázdné pro náhodně zvolené a): "))
-        except ValueError:
-            a = None
-        try:
-            b = int(input("Zadejte hodnotu b (Nechte prázdné pro náhodně zvolené b): "))
-        except ValueError:
-            b = None
-        ans, time = stopwatch(dh.setKey, g = g, modLength = modLength, a = a, b = b)
+        mod = inp(int, "Zadejte prvočíselné modulo grupy (Nechte prázdné pro náhodně zvolené modulo): ", "Zkuste to znovu: ", skipable=True)
+        if not mod:
+            modLength = inp(int, "Zadejte bitovou délku modula grupy (Nechte prázdné pro délku 2048 b): ", "Zkuste to znovu: ", skipable=True)
+        g = inp(int, "Zadejte hodnotu prvku (Nechte prázdné pro náhodně zvolené g): ", "Zkuste to znovu: ", skipable = True)
+        g = g % mod
+        a = inp(int, "Zadejte hodnotu a (Nechte prázdné pro náhodně zvolené a): ", "Zkuste to znovu: ", skipable = True)
+        a = a % mod
+        b = inp(int, "Zadejte hodnotu b (Nechte prázdné pro náhodně zvolené b): ", "Zkuste to znovu: ", skipable = True)
+        b = b % mod
+        ans, time = stopwatch(dh.setKey, g = g, modLength = modLength, mod = mod, a = a, b = b)
         mod, g, a, b, A, B, keyA, keyB = ans
-        print("mod = %d\ng = %d\na = %d\nb = %d\nA = %d\nB = %d\nkeyA = %d\nkeyB = %d\nOperace trvala %.3f sekund" % (mod, g, a, b, A, B, keyA, keyB, time))
+        print("Bylo zvoleno:\nmod = %d\ng = %d\nAlice vygenerovala a = %d\nBob vygeneroval b = %d\nAlice poslala A = %d\nBob poslal B = %d\nAlice získala klíč: %d\nBob získal klíč: %d\nOperace trvala %.3f sekund" % (mod, g, a, b, A, B, keyA, keyB, time))
+        input("Stiskněte enter pro pokračování.")
 
 
-
-    #elif option == 3:
+    elif option == 3:
         #basics
+        options1 = ("Diskrétní logaritmus", "Největší společný dělitel dvou čísel")
+        option1 = menu("Basics", options1)
+        if option1 == 1:
+            #DisLog
+            print("Diskrétní logaritmus hledá číslo x, pro které platí rovnice ve tvaru: a = b^x (mod n).")
+            a = inp(int, "Zadejte a (Výsledek modulárního mocnění): ")
+            b = inp(int, "Zadejte b (Základ pro mocnění): ")
+            n = inp(int, "Zadejte n (Modulo): ")
+            ans = basics.discreteLogarithm.disLog(a, b, n)
+            print("x = %d" % ans)
+            input("Stiskněte enter pro pokračování.")
+
+        elif option1 == 2:
+            #GCD
+            a = inp(int, "Zadejte první číslo: ")
+            b = inp(int, "Zadejte druhé číslo: ")
+            ans = basics.euklidesAlgorithm.euklides(a, b)
+            print("Největší společný dělitel %d a %d je %d" % (a, b, ans))
+            input("Stiskněte enter pro pokračování.")
 
     option = menu("Hlavní menu:", options)
 
